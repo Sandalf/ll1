7bb590087b493a8dc68cab7367a55ef28eab708a
Luis Sandoval <fofonso_97@me.com>
2019-10-08 21:59:22 -0500

    It seems to work

--- ll1.cpp
+++ ll1.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
 #include <stack>
 #include <typeinfo>
+#include <map>
 #include "scanner.hpp"
 
 using namespace std;
+
+token tok;
 
 enum generator
 {
@@ -15,11 +18,15 @@ enum generator
     err
 };
 
-struct Production
+struct Symbol
 {
     token terminal;
     generator generator;
+    bool isTerminal;
+    bool isGenerator;
 };
+
+typedef stack<Symbol> production;
 
 void pgenerator(generator g)
 {
@@ -49,39 +56,110 @@ void pgenerator(generator g)
     }
 }
 
+bool matchtoken(token k) {
+    if (k == tok) {
+        tok = next();
+        return true;
+    }
+    return false;
+}
+
 extern bool ll1(const char *fname)
 {
 
     open(fname);
 
-    generator table[5][7] = {
-                /* (     -    num   +       )      *      $ */
-    /* exp   */ { exp,  exp,  exp,  err,   err,   err,   err},
-    /* exp'  */ { err,  err,  err,  exp_,  exp_,  err,   exp_},
-    /* term  */ { term, term, term, err,   err,   err,   err},
-    /* term' */ { err,  err,  err,  term_, term_, term_, term_},
-    /* fact  */ { fact, fact, fact, err,   err,   err,   err}
+    // generator table[5][7] = {
+    //             /* (     -    num   +       )      *      $ */
+    // /* exp   */ { exp,  exp,  exp,  err,   err,   err,   err},
+    // /* exp'  */ { err,  err,  err,  exp_,  exp_,  err,   exp_},
+    // /* term  */ { term, term, term, err,   err,   err,   err},
+    // /* term' */ { err,  err,  err,  term_, term_, term_, term_},
+    // /* fact  */ { fact, fact, fact, err,   err,   err,   err}
+    // };
+
+    production p1;
+    p1.push((Symbol){ .generator = term, .isGenerator = true });
+    p1.push((Symbol){ .generator = exp, .isGenerator = true });
+
+    production p2;
+    p2.push((Symbol){ .terminal = _sum, .isTerminal = true });
+    p2.push((Symbol){ .generator = term, .isGenerator = true });
+    p2.push((Symbol){ .generator = exp, .isGenerator = true });
+
+    production p3;
+
+    production p4;
+    p4.push((Symbol){ .generator = fact, .isGenerator = true });
+    p4.push((Symbol){ .generator = term_, .isGenerator = true });
+
+    production p5;
+    p5.push((Symbol){ .terminal = _mult, .isTerminal = true });
+    p5.push((Symbol){ .generator = fact, .isGenerator = true });
+    p5.push((Symbol){ .generator = term_, .isGenerator = true });
+
+    production p6;
+
+    production p7;
+    p7.push((Symbol){ .terminal = _lpar, .isTerminal = true });
+    p7.push((Symbol){ .generator = exp, .isGenerator = true });
+    p7.push((Symbol){ .terminal = _rpar, .isTerminal = true });
+
+    production p8;
+    p8.push((Symbol){ .terminal = _min, .isTerminal = true });
+    p8.push((Symbol){ .generator = fact, .isGenerator = true });
+
+    production p9;
+    p9.push((Symbol){ .terminal = _num, .isTerminal = true });
+
+    /* Error Production */
+    production pE;
+    pE.push((Symbol){ .terminal = _err, .isTerminal = true });
+
+    production table[5][7] = {
+                /* (  -   num  +   )   *  $ */
+    /* exp   */ { p1, p1, p1, pE, pE, pE, pE },
+    /* exp'  */ { pE, pE, pE, p2, p3, pE, p3 },
+    /* term  */ { p4, p4, p4, pE, pE, pE, pE },
+    /* term' */ { pE, pE, pE, p6, p6, p5, p6 },
+    /* fact  */ { p7, p8, p9, pE, pE, pE, pE }
     };
 
-    token t = next();
-    stack<generator> s;
-    generator initSymbol;
+    tok = next();
+    stack<Symbol> s;
+    Symbol initSymbol;
+    initSymbol.generator = exp;
+    initSymbol.isGenerator = true;
 
-    s.push(exp);
+    s.push(initSymbol);
 
-    generator x = s.top();
-    generator pi = table[x][t];
-
-    pgenerator(pi);
-
-    if (x == err) {
-        close();
-        return false;
+    Symbol x = s.top();
+    if (x.isTerminal) {
+        if (matchtoken(x.terminal));
+        else return false;
+    } else {
+        production pi = table[x.generator][tok];
+        Symbol sim = pi.top();
+        if (sim.isTerminal && sim.terminal == _err) return false;
+        else {
+            production piInverted;
+            while (!pi.empty())
+            {
+                piInverted.push(p1.top());
+                pi.pop();
+            }            
+        }
     }
+    // production pi = table[x][t];
 
-    
+    // pgenerator(pi);
+
+    // if (x == err) {
+    //     close();
+    //     return false;
+    // }    
 
     close();
 
-    return t != _eof;
+    return tok != _eof;
 }
